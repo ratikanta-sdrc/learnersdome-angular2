4f6a2384be6e70001a9d1d276354ec759b5ecf3e
ratikanta-sdrc <ratikanta@sdrc.co.in>
2017-10-22 21:57:32 +0530

    router, footer, flex-layout and home components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './header/header.component'
 
 import { MatToolbarModule } from '@angular/material';
-import { HeaderComponent } from './header/header.component'
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component'
+
+import { RouterModule, Routes } from '@angular/router'
+
+const appRoutes: Routes = [
+  
+  {
+    path: '',
+    component: LandingComponent,
+    data: { title: 'Learner\'s Dome' }
+  },
+  { path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  { path: '**', component: LandingComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    MatToolbarModule
+    MatToolbarModule,
+    FlexLayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
